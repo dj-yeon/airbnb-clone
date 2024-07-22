1bf9bb7a8a6bb4da5db44fd16fea1e88635c8708
Dongju Yeon <dongju1497@gmail.com>
2024-07-22 22:25:46 +0900

    feat: Category UI created

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,26 +1,29 @@
 'use client';
 
-import { User } from '@prisma/client';
 import Container from '../Container';
+import Categories from './Categories';
 import Logo from './Logo';
 import Search from './search';
 import UserMenu from './UserMenu';
+import { SafeUser } from '@/app/types/SafeUser';
 
 interface NavbarProps {
-  currentUser?: User | null;
+  currentUser?: SafeUser | null;
 }
 
 const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
   return (
     <div className="fixed w-full bg-white z-10 shadow-sm">
-      <div className="py-4 border-b-[1px]"></div>
-      <Container>
-        <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
-          <Logo />
-          <Search />
-          <UserMenu currentUser={currentUser} />
-        </div>
-      </Container>
+      <div className="py-4 border-b-[1px]">
+        <Container>
+          <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
+            <Logo />
+            <Search />
+            <UserMenu currentUser={currentUser} />
+          </div>
+        </Container>
+      </div>
+      <Categories />
     </div>
   );
 };
